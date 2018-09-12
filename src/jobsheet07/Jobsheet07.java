b834715df3e1f6d672a97458b688b80c159bcc3e
ASUS <ASUS@DESKTOP-K3F0R94>
2018-09-12 13:16:18 +0700

    Dynamic Programming _ Fibonacci

--- src/jobsheet07/Jobsheet07.java
+++ src/jobsheet07/Jobsheet07.java
@@ -16,6 +16,7 @@ public class Jobsheet07 {
         tampilJudul(identitas);
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
     }
     private static int tampilInput() {
         
@@ -38,6 +39,9 @@ public class Jobsheet07 {
         }
         
         return hasil[n-1];
+    }
+    private static void tampilHasil(int n, BigInteger hasil) {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
     }
     
 }
